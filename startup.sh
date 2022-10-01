e2918d08c0ecbfbcb700482a83be7c80be789314
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-10-02 00:18:13 +0900

    Add cash in actions yml

--- startup.sh
+++
@@ -1,6 +0,0 @@
-rm -rf /usr/local/etc/php/conf.d/opcache-recommended.ini
-rm -rf /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
-service nginx reload
-
-php artisan key:generate
-php artisan optimize
